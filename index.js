0d5769506e0b08ed13f6e2438b116bc07a5fa40f
jaksonicm <jaksonicm@hotmail.com>
2023-04-08 14:11:05 -0300

    novo

---
+++ index.js
@@ -0,0 +1,9 @@
+const dino=document.querySelector('.dino')
+const background=document.querySelector('.background')
+
+function pegareventoteclado(event)
+{
+    if(event.keycode===32){
+        console.log("tecla espaço pressionado")
+    }
+}
